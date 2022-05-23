ce385ad0961203005c00cd5bd2fe8b51649dd2b6
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2022-05-23 09:44:35 +0100

    Create TestarCoisas.java

---
+++ 1ºAno/2ºSemestre/POO/src/TestarCoisas.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class TestarCoisas {
+    public static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i == count; i++) {
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
