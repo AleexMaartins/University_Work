8860017cc7b0aa88ec3e84315d9049de7cf3a348
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2023-02-22 19:32:36 +0000

    update

--- 1ºAno/2ºSemestre/POO/src/Aula02/Ex10.java
+++ 1ºAno/2ºSemestre/POO/src/Aula02/Ex10.java
@@ -1,17 +1,68 @@
 package Aula02;
+
 import java.util.Scanner;
-import java.util.ArrayList;
+
 public class Ex10 {
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Insert real numbers, to stop insert the first number sent:");
-    double number = input.nextDouble();
-    ArrayList<Double> arrayList = new ArrayList<Double>();
+        Scanner input = new Scanner(System.in);
+        
+        double valor;
+        double primeiroValor = 0;
+        double soma = 0;
+        int contador = 0;
+        boolean check = true;
+
+        System.out.println();
+        System.out.println("(Para terminar a contagem insira o primeiro numero enviado, este ultimo nao afetara a contagem,soma,minimo,maximo ou media)");
+        System.out.print("Digite um número real: ");
+        while(check){
+            try {
+                primeiroValor = input.nextDouble();
+                check = false;
+
+            } catch (Exception e) {
+                System.out.println("INSIRA UM NUMERO REAL!!!!!!!1!!1");
+                System.out.print("Digite um número real: ");
+                input.nextLine();
+            }
+        }
+        
+        valor = primeiroValor;
+        double maximo = valor ;
+        double minimo = valor;
+        do{
+            soma += valor;
+            contador++;
+            if (valor > maximo) {
+                maximo = valor;
+            }
+            if (valor < minimo) {
+                minimo = valor;
+            }
+            check = true;
+            System.out.print("Digite outro número real: ");
+
+            while(check){
+                try {
+                    valor = input.nextDouble();
+                    check = false;
     
-    do{
-        arrayList.add(number);
-        number = input.nextDouble();
-    }while(number != arrayList.get(0));
+                } catch (Exception e) {
+                    System.out.println("Por favor insira um numero real!");
+                    System.out.print("Digite outro número real: ");
+                    input.nextLine();
+                }
+            }
+        }while (valor != primeiroValor);
+        
+        if (contador == 0) {
+            System.out.println("Nenhum número foi digitado.");
+        }
+            double media = soma / contador;
+            System.out.println("Valor máximo: " + maximo);
+            System.out.println("Valor mínimo: " + minimo);
+            System.out.println("Média: " + media);
+            System.out.println("Total de números lidos: " + contador);
+        
     }
 }
-//FALTA ACABAR
