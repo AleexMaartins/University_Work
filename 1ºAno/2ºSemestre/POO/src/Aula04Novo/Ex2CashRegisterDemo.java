0da45a8fd9fdc65f70f8b82fc1ed5fc5858a95c3
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2023-03-07 12:45:14 +0000

    update

--- 1ºAno/2ºSemestre/POO/src/Aula04Novo/Ex2CashRegisterDemo.java
+++ 1ºAno/2ºSemestre/POO/src/Aula04Novo/Ex2CashRegisterDemo.java
@@ -7,16 +7,19 @@ class Product {
     private String name;
     private double price;
     private int quantity;
+    private double total;
+
 
     @Override
     public String toString() {
-        return "Product [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+        return  name +  "\t"  + price + "\t"+"\t"  + quantity + "\t" +"\t" + total + "\n"  ;
     }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.total= getTotalValue();
     }
 
     public double getTotalValue() {
@@ -52,20 +55,21 @@ class CashRegister {
         // print header
         // for each product in productlist
         // print product.tostring
-        return "CashRegister [listaProdutos=" + listaProdutos + "]";
+        System.out.println("Product" + "\t" +"\t"  +"Price" + "\t" +"\t"  +"Quantity" + "\t"  +"Total" );
+        return listaProdutos + "" ;
     }
 }
 
 public class Ex2CashRegisterDemo {
 
     public static void main(String[] args) {
-
+        
         // Cria e adiciona 5 produtos
         CashRegister cr = new CashRegister();
-        cr.addProduct(new Product("Book", 9.99, 3));
-        cr.addProduct(new Product("Pen", 1.99, 10));
+        cr.addProduct(new Product("Book      ", 9.99, 3));
+        cr.addProduct(new Product("Pen       ", 1.99, 10));
         cr.addProduct(new Product("Headphones", 29.99, 2));
-        cr.addProduct(new Product("Notebook", 19.99, 5));
+        cr.addProduct(new Product("Notebook  ", 19.99, 5));
         cr.addProduct(new Product("Phone case", 5.99, 1));
 
         // TO DO: Listar o conteúdo e valor total
