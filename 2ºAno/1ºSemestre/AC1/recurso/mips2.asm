3dbf4bb105a4ea94f13567b4535ef43b37b862e3
Alexandre Martins <75785342+AleexMaartins@users.noreply.github.com>
2024-02-10 15:52:16 +0000

    update

---
+++ 2ºAno/1ºSemestre/AC1/recurso/mips2.asm
@@ -0,0 +1,17 @@
+#Mapa de registos
+#f1 : $s0
+#k : $s1
+#av : $s2
+#i : $s3
+#res : $t0
+
+	.data
+str:	.asciiz "Invalid argc"
+	.eqv print_string,4
+	.eqv print_int10, 1
+	.eqv SIZE,15
+	.text
+	
+func2:
+
+	
